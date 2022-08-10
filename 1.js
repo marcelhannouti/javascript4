519406b59231a6e953e9c51dea47d6fa8d8e3e65
marc <marc.hannouti360@gmail.com>
2022-08-10 12:52:58 +0200

    lnln

--- 1.js
+++ 1.js
@@ -2,3 +2,18 @@ function arrow(nombre,apellidos) {
     return  nombre + " "+ apellidos
 }
 console.log(arrow("marc","hannouti"));
+
+
+function bool (respuesta) {
+    console.log(respuesta)
+}
+
+bool(true)
+
+function parametro (...param){
+param.forEach((param) =>{
+    console.log(param)
+})
+}
+parametro("dsv","dv","dvdsvv","advs","dvv")
+
